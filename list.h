fa119055e9d3cc6e8d58a0f903cfeb169cb0eafa
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-09 23:25:14 +1100

    Making list.h work and adding container_of
     * http://www.kroah.com/log/linux/container_of.html

--- list.h
+++ list.h
@@ -1,11 +1,8 @@
 #ifndef _LINUX_LIST_H
 #define _LINUX_LIST_H
 
-#include <linux/types.h>
-#include <linux/stddef.h>
-#include <linux/poison.h>
-#include <linux/const.h>
-#include <linux/kernel.h>
+#include "types.h"
+#include "poison.h"
 
 /*
  * Simple doubly linked list implementation.
