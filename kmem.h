2ab2df73c270a4af7299c71dbb45e09c63f994b8
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-10 01:06:11 +1100

    Adding more memory functions.

--- kmem.h
+++ kmem.h
@@ -5,10 +5,22 @@
 #define _KMEM_H_
 
 #include <stdlib.h>
+#include <string.h>
 
 #define kmalloc(size, ...) malloc(size)
 #define krealloc(ptr, size, ...) realloc(ptr, size)
 #define kmemdup(ptr, size, ...) memdup(ptr, size)
+#define kcalloc(n, size, ...) calloc(n, size)
+#define kzalloc(ptr, size, ...) zalloc(size)
+#define kfree(ptr, ...) free(ptr)
+
+#define memchr_inv(s, c, n) memchr(s, c, n)
+
+inline void* zalloc(size_t s) {
+   void* p = malloc(s);
+   memset(p, 0, s);
+   return p;
+}
 
 inline void* memdup(const void* d, size_t s) {
    void* p; 
